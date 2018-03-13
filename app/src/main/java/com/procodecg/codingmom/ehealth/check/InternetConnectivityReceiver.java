a3f487d6d1fe9832daef8ae226c7066880584439
annisaalif <annisaalif26@gmail.com>
2018-03-13 14:25:37 +0700

    Create InternetConnectivityReceiver.java

---
+++ app/src/main/java/com/procodecg/codingmom/ehealth/check/InternetConnectivityReceiver.java
@@ -0,0 +1,37 @@
+package com.procodecg.codingmom.ehealth.check;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.procodecg.codingmom.ehealth.fragment.BottombarActivity;
+
+/**
+ * Created by idedevteam on 3/13/18.
+ */
+
+public class InternetConnectivityReceiver extends BroadcastReceiver {
+    public InternetConnectivityReceiver(){
+        super();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager
+                .getActiveNetworkInfo();
+
+        BottombarActivity ba = BottombarActivity.instance;
+
+        // Check internet connection and accrding to state change the
+        // text of activity by calling method
+        if (networkInfo != null && networkInfo.isConnected()) {
+            ba.changeTextStatus(true);
+        } else {
+            ba.changeTextStatus(false);
+        }
+    }
+}
