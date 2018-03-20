d152388e0210136552e27d892ceb287d00dc3a6b
inev <ine.sena@gmail.com>
2018-03-20 15:01:01 +0700

    add

--- app/src/main/java/com/procodecg/codingmom/ehealth/utils/Policy.java
+++ app/src/main/java/com/procodecg/codingmom/ehealth/utils/Policy.java
@@ -1,13 +1,10 @@
 package com.procodecg.codingmom.ehealth.utils;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
 import android.view.View;
-import android.widget.TextView;
 
 import com.procodecg.codingmom.ehealth.R;
 
@@ -17,39 +14,11 @@ import com.procodecg.codingmom.ehealth.R;
 
 public class Policy extends AppCompatActivity {
 
-    private TextView txtTitle;
-    Typeface fontBold;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //MENGHILANGKAN TOOLBAR
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.policy);
 
-        txtTitle = (TextView) findViewById(R.id.txt_title);
-        txtTitle.setText("Privacy Policy");
-
-        fontBold = Typeface.createFromAsset(getAssets(),"font1bold.ttf");
-        txtTitle.setTypeface(fontBold);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        toolbar.setNavigationIcon(R.drawable.xblue);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), Setting.class));
-            }
-        });
 
         //set popup window
         DisplayMetrics dm = new DisplayMetrics();
@@ -58,6 +27,13 @@ public class Policy extends AppCompatActivity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.92),(int)(height*.88));
+        getWindow().setLayout((int)(width*.80),(int)(height*.60));
+
     }
+
+    //set close button
+    public void closeToMain(View view) {
+        startActivity(new Intent(getApplicationContext(), Setting.class));
+    }
+
 }
